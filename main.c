5a69c0995b0626ee5aacd101091a65603c03d733
Noah Törnqvist <101272358+NoahTornqvist@users.noreply.github.com>
2024-03-17 21:20:15 +0100

    Update main.c with comments

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-//
-//  main.c
-//  Extension
-//
-//  Created by Dave Hayden on 7/30/14.
-//  Copyright (c) 2014 Panic, Inc. All rights reserved.
-//
+/**
+ * @file event_handler.c
+ * @brief Implementation file for the event handler function.
+ * @details This file contains the implementation of the event handler function
+ *          for the Extension project, which initializes the game and sets up
+ *          the update callback.
+ * @author Noah Törnqvist
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,15 +16,29 @@
 #ifdef _WINDLL
 __declspec(dllexport)
 #endif
+
+/** @brief Event handler function for the Extension project.
+ *  @param playdate Pointer to the PlaydateAPI instance.
+ *  @param event The system event.
+ *  @param arg Additional argument associated with the event.
+ *  @return 0 if event is handled successfully.
+ */
 int eventHandler(PlaydateAPI* playdate, PDSystemEvent event, uint32_t arg)
 {
-	if (event == kEventInit)
-	{
-		setPDPtr(playdate);
-		playdate->display->setRefreshRate(0); // run as fast as possible
-		playdate->system->setUpdateCallback(update, NULL);
-		//setupGame();
-	}
-	
-	return 0;
+    if (event == kEventInit)
+    {
+        // Set the pointer to the PlaydateAPI instance
+        setPDPtr(playdate);
+        
+        // Set the display refresh rate to run as fast as possible
+        playdate->display->setRefreshRate(0);
+        
+        // Set the update callback function
+        playdate->system->setUpdateCallback(update, NULL);
+        
+        // Initialize the game (optional)
+        // setupGame();
+    }
+    
+    return 0;
 }
